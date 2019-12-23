8ce984c7ad4c4732b8dc2039ae507575ea5a2a5a
Michael Meli <mikecmeli@gmail.com>
2019-12-22 22:56:07 -0500

    functional, but worse than random guessing

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ on_pixels = train_images > pixel_threshold
 train_images_threshold = np.zeros_like(train_images)
 train_images_threshold[on_pixels] = 1
 
-im = Image.fromarray(np.reshape(train_images_threshold[0:100,:,:] * 255,(28*100,28)))
-im.save('out.jpeg', "JPEG")
-print(np.max(train_images[0,:,:]), np.min(train_images[0,:,:]))
+# im = Image.fromarray(np.reshape(train_images_threshold[0:100,:,:] * 255,(28*100,28)))
+# im.save('out.jpeg', "JPEG")
+# print(np.max(train_images[0,:,:]), np.min(train_images[0,:,:]))
 nb_matrix = np.zeros((10,train_images.shape[1],train_images.shape[2]))
 counts = np.zeros(10)
 for i in range(10):
@@ -29,5 +29,25 @@ for i in range(10):
     sum = np.sum(train_images[indices,:,:], axis=(0,1))
     nb_matrix[i,:,:] = sum/counts[i]
 
+x_list = []
 
+# need to vectorize
+for i in range(1000): # need to change this
+    on_pixels = train_images_threshold[i] == 1
+    off_pixels = train_images_threshold[i] == 0
+    probs = np.zeros((10,train_images.shape[1],train_images.shape[2]))
+    for j in range(10): # need to change this
+        probs[j,on_pixels] = nb_matrix[j,on_pixels] + epsilon
+        probs[j,off_pixels] = 1 - nb_matrix[j,off_pixels] + epsilon
+    
+    x_list.append(np.argmax(np.prod(probs, axis=(0,1)))) # product here will probably underflow
 
+x_list = np.asarray(x_list)
+
+print(train_labels[0:1000])
+print(x_list)
+
+equal_indices = train_labels[0:1000] == x_list
+print(equal_indices)
+equal_amount = equal_indices.sum()
+print(equal_amount/1000)
