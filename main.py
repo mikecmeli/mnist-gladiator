a1fd5af04b34aac568a58ebf42478c5f0fceae6a
Michael Meli <mikecmeli@gmail.com>
2019-12-22 22:32:45 -0500

    thresholding images

--- main.py
+++ main.py
@@ -13,9 +13,12 @@ train_labels = mnist.train_labels()
 test_images = mnist.test_images()
 test_labels = mnist.test_labels()
 
+on_pixels = train_images > pixel_threshold
 
+train_images_threshold = np.zeros_like(train_images)
+train_images_threshold[on_pixels] = 1
 
-im = Image.fromarray(np.reshape(train_images[0:100,:,:],(28*100,28)))
+im = Image.fromarray(np.reshape(train_images_threshold[0:100,:,:] * 255,(28*100,28)))
 im.save('out.jpeg', "JPEG")
 print(np.max(train_images[0,:,:]), np.min(train_images[0,:,:]))
 nb_matrix = np.zeros((10,train_images.shape[1],train_images.shape[2]))
